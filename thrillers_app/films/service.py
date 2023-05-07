8005af4e12bf8deebfe4b98a1c6b21b6e37bf5ad
Toby <spaceocean@outlook.com>
2023-05-07 16:40:59 +0500

    edit service

--- thrillers_app/films/service.py
+++ thrillers_app/films/service.py
@@ -3,17 +3,17 @@ from films.models import Film, Country
 
 def service_analytics():
     films = Film.objects.all()
-    country = Country.objects.all()
+    countries = Country.objects.all()
     analytics_country = []
     all_rating = 0
-    for i in country:
-        num_country = Country.objects.get(pk=i.pk)
-        f = Film.objects.filter(country=num_country)
-        res = len(films) / 100 * len(f)
-        a = (res, i.title, len(f))
-        analytics_country.append(a)
-    for i in films:
-        all_rating += i.rating
+    for country in countries:
+        num_country = Country.objects.get(pk=country.pk)
+        how_many_countries = Film.objects.filter(country=num_country)
+        res = 100 * len(how_many_countries) / len(films)
+        data_country = (round(res, 2), country.title, len(how_many_countries))
+        analytics_country.append(data_country)
+    for film in films:
+        all_rating += film.rating
     middle_rating = round((all_rating / len(films)), 2)
     analytics_country = sorted(analytics_country, reverse=True)
     return middle_rating, analytics_country
